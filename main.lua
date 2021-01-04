90d8f513622ac8d5355415985b7d1d25ad6b0fe8
Zyro <60398669+ZyroCrimson@users.noreply.github.com>
2021-01-03 19:45:56 -0500

    Update main.lua
    added another tiny change

--- main.lua
+++ main.lua
@@ -2,81 +2,94 @@
 
 local Convert = {}
 
-function Convert.UDim2ToVector2(udim2)
-	return Vector2.new(udim2.X.Offset, udim2.Y.Offset)
+-- define what the functions return using type script
+-- define the parameters that are needed
+-- check whether the passed arguments are correct
+
+local function checkType(parameter, _type)
+   if typeof(parameter) ~= _type then
+      local got = typeof(parameter)
+       warn ( string.format("Invalid type \"%s.\" (\"%s\" expected)", got, _type) )
+      return nil
+   end
 end
 
-function Convert.Vector2ToUDim2(vector2)
-	return UDim2.fromOffset(vector2.X, vector2.Y)
+function Convert.UDim2ToVector2(udim2: UDim2) : Vector2
+   checkType(udim2, "UDim2")
+
+   return Vector2.new(udim2.X.Offset, udim2.Y.Offset)
 end
 
-function Convert.StringToBool(str)
-	if str == "true" or str == "false" then
-		local boolNew
-		
-		if str == "true" then
-			boolNew = true
-		elseif str == "false" then
-			boolNew = false
-		end
-		
-		return boolNew
-	else
-		warn("Parameter must be a string!")
-		return nil
-	end
+function Convert.Vector2ToUDim2(vector2: Vector2) : UDim2
+   checkType(vector2, "Vector2")
+
+   return UDim2.fromOffset(vector2.X, vector2.Y)
 end
 
-function Convert.BoolToString(bool)
-	if bool == true or bool == false then
-		local stringNew
-		
-		if bool == true then
-			stringNew = "true"
-		elseif bool == false then
-			stringNew = "false"
-		end
-		
-		return stringNew
-	else
-		warn("Parameter must be a boolean!")
-		return "nil"
-	end
+function Convert.StringToBool(str: string) : boolean
+  checkType(str, "string")
+
+  if str == "true" or str == "false" then
+      if str == "true" then
+         return true
+  elseif str == "false" then
+	 return false
+      end
+  else
+      warn("Parameter must be either \"true\" or \"false\"")
+      return nil
+   end
 end
 
-function Convert.Color3ToBrickColor(color3)
-	return BrickColor.new(color3)
+function Convert.BoolToString(bool: boolean) : string
+   checkType(bool, "boolean")
+
+  if bool == true or bool == false then
+      if bool == true then
+          return "true"
+      elseif bool == false then
+	  return "false"
+      end
+   else
+      warn("Parameter must be either \"true\" or \"false\"!")
+      return "nil"
+   end
 end
 
-function Convert.BrickColorToColor3(brickcolor)
-	return brickcolor.color
+function Convert.Color3ToBrickColor(color3: Color3) : BrickColor
+   checkType(color3, "Color3")
+
+   return BrickColor.new(color3)
 end
 
-function Convert.Vector3int16ToVector3(vector16)
-	return Vector3.new(vector16.X, vector16.Y, vector16.Z)
+function Convert.BrickColorToColor3(brickcolor: BrickColor) : Color3
+   checkType(brickcolor, "BrickColor")
+
+   return brickcolor.Color
 end
 
-function Convert.Vector3ToVector3int16(vector3)
-	return Vector3int16.new(vector3.X, vector3.Y, vector3.Z)
+function Convert.Vector3int16ToVector3(vector16: Vector3int16) : Vector3
+   checkType(vector16, "Vector3int16")
+
+   return Vector3.new(vector16.X, vector16.Y, vector16.Z)
 end
 
-function Convert.Vector3ToCFrame(vector3)
-	if (not vector3.X) or (not vector3.Y) or (not vector3.Z) then
-		warn("Parameter must be a Vector3!")
-		return
-	else
-		return CFrame.new(vector3)
-	end
+function Convert.Vector3ToVector3int16(vector3: Vector3) : Vector3int16
+   checkType(vector3, "Vector3")
+
+   return Vector3int16.new(vector3.X, vector3.Y, vector3.Z)
 end
 
-function Convert.CFrameToVector3(cframe)
-	if (not cframe.X) or (not cframe.Y) or (not cframe.Z) then
-		warn("Parameter must be a CFrame!")
-		return
-	else
-		return Vector3.new(cframe.X, cframe.Y, cframe.Z)
-	end
+function Convert.Vector3ToCFrame(vector3: Vector3) : CFrame
+  checkType(vector3, "Vector3")
+
+   return CFrame.new(vector3)
 end
 
-return Convert
+function Convert.CFrameToVector3(cframe: CFrame) : Vector3
+   checkType(cframe, "CFrame")
+
+   return Vector3.new(cframe.X, cframe.Y, cframe.Z)
+end
+
 return Convert
